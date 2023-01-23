0e874960e6ad36399e07b34e7633e2e91d119857
Diksha Yadav <yadav.diksha1503@gmail.com>
2023-01-23 19:50:25 +0530

    Create Binary Search.java

---
+++ Binary Search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int len = nums.length, low = 0, high = len-1;
+        while(low <= high){
+            int mid = (high+low)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(target > nums[mid]){
+                low = mid+1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
